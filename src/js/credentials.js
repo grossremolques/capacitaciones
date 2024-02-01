638e51c6fa2964c49751429e917b24867d174fa7
grossremolques <katherin.guerrero@grossremolques.com>
2024-02-01 07:17:53 -0300

    Ready to deploy

---
+++ src/js/credentials.js
@@ -0,0 +1,3 @@
+const spreadsheetId = '1QWbYHFvfY4SL1atMa89ZjF6w3yDYJJ2P6Ra6QAVrGT4' //https://docs.google.com/spreadsheets/d/1QWbYHFvfY4SL1atMa89ZjF6w3yDYJJ2P6Ra6QAVrGT4
+const API_KEY = 'AIzaSyAdu2tV6VLhpEzRNoZedn8-tcwfhyey96s';
+const CLIENT_ID = '619237997160-b8lenl505tptgf05csuvup6d4e5i2tkk.apps.googleusercontent.com'
